8abcd280268ea947d119b46b93e9a2387b9fd42e
JulioCesarMarques2 <juliopereira@aluno.senai.br>
2023-08-03 15:10:41 -0300

    Enviar

--- Fundamentos/src/Atividade01/instalacao.java
+++ Fundamentos/src/Atividade01/instalacao.java
@@ -9,34 +9,28 @@ public class instalacao {
         if (kmh < 500) {
             double valor = 0.40 * (kmh - 500);
             System.out.println("o valor da instalacao abaixo e = " + valor);
-        } else {
+
             int kmh2 = 700;
             String tipoInstalacao2 = "r";
             if (kmh2 > 500) {
                 double valor2 = 0.65 * (kmh2 - 500);
                 System.out.println("o valor da instalacao acima e = " + valor2);
 
-            }
+                int kmh3 = 500;
+                String tipoComercial = "c";
+                if (kmh3 < 1000) {
+                    double valor3 = 0.55 * (kmh3 - 1000);
+                    System.out.println("o valor do comercial abaixo e = " + valor3);
 
-            int kmh3 = 500;
-            String tipoComercial = "c";
-            if (kmh3 < 1000) {
-                double valor3 = 0.55 * (kmh3 - 1000);
-                System.out.println("o valor do comercial abaixo e = " + valor3);
-            }
-
-        }else {
-                        int kmh4 = 1200;
+                    int kmh4 = 1200;
                     String tipoComercial2 = "c";
                     if (kmh4 > 1000) {
                         double valor4 = 0.60 * (kmh4 - 1000);
-                        System.out.println("o valor do comercial acima e = " + valor4);}
-                }
-                    
+                        System.out.println("o valor do comercial acima e = " + valor4);
 
                         int kmh5 = 1500;
                         String tipoindustrial = "i";
-                        if (kmh5 < 5000) {
+                        if (kmh < 5000) {
                             double valor5 = 0.55 * (kmh5 - 5000);
                             System.out.println("o valor do industrial abaixo e = " + valor5);
 
@@ -49,8 +43,8 @@ public class instalacao {
                         }
                     }
 
+                }
+            }
+        }
     }
-}
-}
-}
 }
