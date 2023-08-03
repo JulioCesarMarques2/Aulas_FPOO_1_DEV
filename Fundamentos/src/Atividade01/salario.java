be610426e0974c2f870c83579379afeca5f3a11b
JulioCesarMarques2 <juliopereira@aluno.senai.br>
2023-08-03 13:46:29 -0300

    testando

--- Fundamentos/src/Atividade01/salario.java
+++ Fundamentos/src/Atividade01/salario.java
@@ -3,9 +3,14 @@ package Atividade01;
 public class salario {
 
     public static void main(String[] args) {
-        double salario = 1350.00 ;
-        double valor = 10 / (salario);
+        double salario = 1350.00;
+        double valor = 13.500 + (salario);
         System.out.println("Desconto acima de 1350.00 = " + valor);
 
+        double salario2 = 1350.00;
+        double valor2 = 20.250 + (salario2);
+        System.out.println("Desconto abaixo de 1350.00 = " + valor2);
+        System.out.println("Teste");
     }
+
 }
